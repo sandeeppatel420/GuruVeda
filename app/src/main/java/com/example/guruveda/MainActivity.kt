4c1cf131e2c3fb2b99191649bab55a47011c5b0b
sande <sandeeppatel7762@gmail.com>
2025-03-28 13:34:22 +0530

    buttomNavigation

--- app/src/main/java/com/example/guruveda/MainActivity.kt
+++ app/src/main/java/com/example/guruveda/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.guruveda
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.guruveda.Fragment.CourseFragment
+import com.example.guruveda.Fragment.HomeFragment
+import com.example.guruveda.Fragment.ProfileFragment
+import com.example.guruveda.Fragment.TestFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bottomNav = findViewById(R.id.bottomNav)
+        loadFragment(HomeFragment())
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_icon -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.card_icon-> {
+                    loadFragment(TestFragment())
+                    true
+                }
+                R.id.myOrder_icon-> {
+                    loadFragment(CourseFragment())
+                    true
+                }
+                R.id. profile_icon-> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+
+                else -> {
+                    return@setOnItemSelectedListener false
+                }
+            }
         }
+    }
+
+    private  fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout_container, fragment)
+        transaction.commit()
     }
 }
