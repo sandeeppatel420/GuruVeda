56ae872299f51e3bf88df0886a9ca43467994e1a
Ravi <raviyuvraj9546@gmail.com>
2025-04-12 14:18:10 +0530

    video get

--- app/src/main/java/com/example/guruveda/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/guruveda/Fragment/HomeFragment.kt
@@ -1,10 +1,13 @@
 package com.example.guruveda.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.guruveda.AllCoursesActivity
 import com.example.guruveda.R
 
 
@@ -13,8 +16,13 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_home, container, false)
+       val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val textView = view.findViewById<TextView>(R.id.allCouresHome)
+        textView.setOnClickListener {
+            val intent = Intent(requireContext(), AllCoursesActivity::class.java)
+            startActivity(intent)
+        }
+        return view
     }
 
 
---
+++ app/src/main/java/com/example/guruveda/ViewModel/VideoGetViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.guruveda.ViewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.guruveda.DataModel.VideoModel
+import com.google.firebase.firestore.FirebaseFirestore
+
+class VideoGetViewModel(): ViewModel() {
+    val videoLiveData = MutableLiveData<List<VideoModel>>()
+    val db = FirebaseFirestore.getInstance()
+    fun getVideos(courseId: String) {
+        db.collection("courses").document(courseId).collection("videos").get()
+            .addOnSuccessListener {
+                val videoList = ArrayList<VideoModel>()
+                for (document in it) {
+                    val video = document.toObject(VideoModel::class.java)
+                    videoList.add(video)
+                }
+                videoLiveData.value = videoList
+            }
+            .addOnFailureListener {
+                videoLiveData.value = null
+            }
+
+    }
+}
